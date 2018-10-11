376ab88f3c9808e883123516529a3c01ac48cd49
tang-suzi <tangtang960225@163.com>
2018-10-11 14:42:20 +0800

    init

--- 验证码/js/拓展.js
+++ 验证码/js/拓展.js
@@ -2,9 +2,9 @@ var codeBox = document.getElementById('codeBox');
 
 function queryCode() {
     var result = '',
-    areaStr = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-    for (var i = 0; i<4;i++){
-        var ran  = Math.round(Math.random()*(areaStr.length-1));
+        areaStr = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    for (var i = 0; i < 4; i++) {
+        var ran = Math.round(Math.random() * (areaStr.length - 1));
         char = areaStr[ran];
         result += char;
     }
